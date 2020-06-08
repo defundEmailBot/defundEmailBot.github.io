aba9c5b0f8ec8d11a68fd5033a111cf40ae2044e
defundEmailBot <66572820+defundEmailBot@users.noreply.github.com>
2020-06-08 14:05:01 -0500

    Delete info copy.py

--- defundEmailBot.zip/info copy.py
+++
@@ -1,8 +0,0 @@
-# ********** INFO TO CHANGE **********
-# make sure each input is surrounded by " "
-FIRST_NAME = "Jane" 
-LAST_NAME = "Doe"
-YOUR_EMAIL_ADDRESS = "myemail@gmail.com"
-YOUR_EMAIL_PASSWORD = "mypassword"
-# ********** INFO TO CHANGE **********
-
